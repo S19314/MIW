f64ad68c95b1af38d8e97dbbcff07a0d52070bdd
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-04-11 23:11:36 +0300

    MP2 Rabotaet. Punkt 1 Version 3.4

--- MP2/main.py
+++ MP2/main.py
@@ -36,11 +36,13 @@ class Perceptron(object):
 
     def predict(self, X):
         result = np.where(self.net_input(X) >= 0.0, 1, -1)
+        '''
         if(len(X) > 2) :
             print("X")
             print(X)
             print("Perceptron predict result ")
             print(result)
+        '''
         # return np.where(self.net_input(X) >= 0.0, 1, -1)
         return  result
 
@@ -48,28 +50,26 @@ class Classifier(object):
     ppnArray = []
     def __init__(self, ppnArray):
         self.ppnArray = ppnArray.copy()
-
+    '''
     def predict(self, x):
         predictionsArray = []
         quantityPerceptrons = len(self.ppnArray)
 
-        for objectForDefining in x:
+         for objectForDefining in x:
             for i in range(quantityPerceptrons):
               ppn = self.ppnArray[i]
               if (ppn.predict(objectForDefining) == 1 ):
                   predictionsArray.append(i) # quantityPerceptrons # Optional...
 
         return predictionsArray.copy()
-'''
-class DataSetManager(object):
-    
-    def preparingTarget(self, targets, minValueInTargets, maxValueInTargets):
-        if(maxValueInTargets-minValueInTargets == 0):
-            targets.
-            return  targets
-        for i in range(maxValueInTargets-minValueInTargets)
-        return
     '''
+
+    def predict(self, x):
+        quantityPerceptrons = len(self.ppnArray)
+        for i in range(quantityPerceptrons):
+          ppn = self.ppnArray[i]
+          if (ppn.predict(x) == 1):
+              return  i # quantityPerceptrons # Optional...
 
 class PerceptronManager(object):
 
@@ -99,15 +99,7 @@ class PerceptronManager(object):
         return  perceptron
 
 
-
-def showResultsTask1(perceptronManager, perceptron):
-    plot_decision_regions(X=perceptronManager.getData_train_01_subset(),
-                          y=perceptronManager.getTarget_train_01_subset(), classifier=perceptron)
-    plt.xlabel(r'$x_1$')
-    plt.ylabel(r'$x_2$')
-    plt.legend(loc='upper left')
-    plt.show()
-def showResultsTask1_V2(perceptron, classificator):
+def showResultsTask1(perceptron, classificator):
     plot_decision_regions(X=perceptron.getData_train_01_subset(),
                           y=perceptron.getTarget_train_01_subset(), classifier=classificator)
     plt.xlabel(r'$x_1$')
@@ -121,7 +113,8 @@ def main():
     # print(datasets.load_iris())
     iris = datasets.load_iris()
     # iris_data = iris.data # zrobić i dla calego zbioru data.
-    iris_data = iris.data[:, [2, 3]]
+#    iris_data = iris.data[:, [2, 3]]
+    iris_data = iris.data
     iris_target = iris.target
     print(iris_data)
     print()
@@ -135,66 +128,16 @@ def main():
 
     classificator = Classifier(perceptrons)
 
-    for i in range(len(perceptrons)):
-        print("I: ")
-        print(i)
-        print("Data")
-        print(perceptrons[i].getData_train_01_subset())
-        print("Target")
-        print(perceptrons[i].getTarget_train_01_subset())
+    correctAnswers = 0
+    for i in range(len(iris_data)):
+        perceptronId = classificator.predict(iris_data[i])
+        if(iris_target[i] == perceptronId) :
+            correctAnswers = correctAnswers + 1
 
-    for perc in perceptrons:
-        showResultsTask1_V2(perc, classificator)
-    '''
-    
-    plot_decision_regions(X=perceptrons[0].getData_train_01_subset(),
-                          y=perceptrons[0].getTarget_train_01_subset(), classifier=classificator)
-    plt.xlabel(r'$x_1$')    
-    plt.ylabel(r'$x_2$')
-    plt.legend(loc='upper left')
-    plt.show()
-    '''
-    '''
-    for perc in perceptrons:
-        showResultsTask1(perceptronManager, perc)
-    '''
-    '''
-    data_train, data_test, target_train, target_test = train_test_split(iris_data, iris_target, test_size=0.3, random_state=1, stratify=iris_target)
-
-    data_train_01_subset = data_train
-    target_train_01_subset = target_train
-    target_train_01_subset[(target_train != 2)] = -1
-    target_train_01_subset[(target_train == 2)] = 1
-    print(target_train_01_subset)
-
-   
-    ppn.fit(data_train_01_subset, target_train_01_subset)
-    '''
-    '''
-    # X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
-    # y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
-    # w perceptronie wyjście jest albo 1 albo -1
-    '''
-    '''
-    # plot_decision_regions(X=data_train_01_subset, y=target_train_01_subset, classifier=perceptrons[0])
-    plot_decision_regions(X=perceptronManager.getData_train_01_subset(), y=perceptronManager.getTarget_train_01_subset(), classifier=perceptrons[0])
-    plt.xlabel(r'$x_1$')
-    plt.ylabel(r'$x_2$')
-    plt.legend(loc='upper left')
-    plt.show()
-'''
+    print("Number of objects: " + str(len(iris_data)))
+    print("Correct answers: " + str(correctAnswers))
+    print("Accurancy " + str(correctAnswers/len(iris_data)))
+# showResultsTask1(perc, classificator)
 
 if __name__ == '__main__':
     main()
-'''
-
-print("initialization")
-# print(datasets.load_iris())
-iris = datasets.load_iris()
-# iris_data = iris.data # zrobić i dla calego zbioru data.
-iris_data = iris.data[:, [2,3]]
-iris_target = iris.target
-print(iris_data)
-print()
-print(iris_target)
-'''
